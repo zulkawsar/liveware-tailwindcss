ce816bb9a92be524ca28d2a04fd4e44cfa30ff86
zulkawsar <zulkawsar@gmail.com>
2021-10-16 00:05:03 +0600

    styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,36 +7,15 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div class="flex-shrink-0">
-            <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
-          </div>
-          <div>
-            <div class="text-xl font-medium text-black">ChitChat</div>
-            <p class="text-gray-500">You have a new message!</p>
-          </div>
-        </div>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                
+        <div class="container mx-auto">
+            <h1 class="text-purple-600 hover:text-red-500 font-bold mx-auto text-center">Standard Contact Form</h1>
+            <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-amber-50 to-amber-100 p-10">
+              <div class="text-center text-5xl font-extrabold leading-none tracking-tight">
+                <span class="decoration-clone bg-clip-text text-transparent bg-gradient-to-b from-amber-400 to-red-500">
+                  Hello<br>
+                  World
+                </span>
+              </div>
             </div>
         </div>
     </body>
